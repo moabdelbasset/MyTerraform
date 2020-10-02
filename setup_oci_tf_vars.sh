8846a52d36620bb143692e4d6a467162cbf8f835
moabdelbasset <59439627+moabdelbasset@users.noreply.github.com>
2020-10-02 13:59:46 +0200

    Create setup_oci_tf_vars.sh

---
+++ setup_oci_tf_vars.sh
@@ -0,0 +1,12 @@
+# This is the original file will be updated according to the OCI values
+export TF_VAR_user_ocid=""
+export TF_VAR_tenancy_ocid=""
+export TF_VAR_compartment_ocid=""
+export TF_VAR_fingerprint=""
+export TF_VAR_private_key_path=""
+export TF_VAR_region=""
+
+export TF_VAR_region1=""
+export TF_VAR_region2=""
+export TF_VAR_private_key_oci=""
+export TF_VAR_public_key_oci=""
